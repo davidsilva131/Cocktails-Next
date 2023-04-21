a7087d28977fe33448d57389fe124705286a01a3
davidsilva131 <davidsilvac131@gmail.com>
2023-04-21 08:15:50 -0500

    Fix error

--- src/pages/ventas.jsx
+++ src/pages/ventas.jsx
@@ -1,12 +1,33 @@
 import PageLayout from '@/components/PageLayout'
+import SellingTable from '@/components/SellingTable'
+import SellingsModal from '@/components/SellingsModal'
+import { getAllCocktails } from '@/services/cocktailsActions'
+import { getInventory } from '@/services/inventoryActions'
+import { getAllSellings } from '@/services/sellingsActions'
+import { useState } from 'react'
 
 /* eslint-disable space-before-function-paren */
-export default function Ventas() {
+export default function Ventas({ sellings, allData }) {
+  const [sellingsData, setSellingsData] = useState(sellings)
+  const [data, setData] = useState(allData)
   return (
     <>
       <PageLayout title='Ventas' />
-      <div>Ventas</div>
-
+      <SellingsModal data={data} />
+      <SellingTable data={sellingsData} setData={setSellingsData} />
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const sellings = await getAllSellings()
+  const cocktailsData = await getAllCocktails()
+  const inventoryData = await getInventory()
+  const allData = [...cocktailsData, ...inventoryData]
+  return {
+    props: {
+      sellings,
+      allData
+    }
+  }
 }
